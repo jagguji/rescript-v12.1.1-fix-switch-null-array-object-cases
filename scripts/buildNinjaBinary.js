8f05eea5b9e7d12e86d39649dd2198c431ebdb84
roshan.chourasiya <roshan.chourasiya@juspay.in>
2026-02-26 19:23:51 +0530

    updated the changes

--- scripts/buildNinjaBinary.js
+++ scripts/buildNinjaBinary.js
@@ -10,7 +10,8 @@ const buildCommand = "python3 configure.py --bootstrap --verbose";
 
 if (platform === "win32") {
   // On Windows, the build uses the MSVC compiler which needs to be on the path.
-  execSync(buildCommand, { cwd: ninjaDir });
+  // Explicitly use cmd.exe so MSVC environment variables are inherited.
+  execSync(buildCommand, { stdio: [0, 1, 2], cwd: ninjaDir, shell: "cmd.exe", env: process.env });
 } else {
   if (process.platform === "darwin") {
     process.env.CXXFLAGS = "-flto";
