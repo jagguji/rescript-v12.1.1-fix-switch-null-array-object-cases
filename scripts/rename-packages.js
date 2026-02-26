e416af8724ad25e19c2af5103d9f9ba097fb2199
roshan.chourasiya <roshan.chourasiya@juspay.in>
2026-02-26 20:46:19 +0530

    Fix rename-packages.js to use ES modules

--- scripts/rename-packages.js
+++ scripts/rename-packages.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const version = process.argv[2];
 
 if (!version) {
